570814053908dbca68d846765fe1dd5a339f5db7
iamluismoran <iamluismoran@gmail.com>
2025-07-11 19:13:51 +0200

    estructura css y script del contact page

---
+++ 3. contact/script.js
@@ -0,0 +1,31 @@
+document.getElementById("contactForm").addEventListener("submit", function (e) { e.preventDefault()
+
+    const name = document.getElementById("name").value.trim()
+    const email = document.getElementById("email").value.trim()
+    const message = document.getElementById("message").value.trim()
+    const phone = document.getElementById("phone").value.trim()
+
+    const successMessage = document.getElementById("successMessage");
+    const userName = document.getElementById("userName");
+
+    if (!name || !email || !message) {
+        alert("Please fill in all required fields.")
+        return
+    }
+
+    const phoneRegex = /^[0-9]{9,15}$/;
+    if (!phoneRegex.test(phone)) {
+    alert("Please enter a valid phone number");
+    return;
+    }
+
+    userName.textContent = name
+    successMessage.classList.add("show")
+    
+    
+    this.reset()
+    
+    setTimeout(() => {
+    successMessage.classList.remove("show")
+  }, 5000)
+});
